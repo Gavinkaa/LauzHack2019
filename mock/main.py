5f7ee6bef68b7e8ddaf07fe5920b5a0d4757836c
Gavinkaa <ludovic.burnier@epfl.ch>
2019-11-16 15:06:29 +0100

    mock api can be used with json

--- mock/main.py
+++ mock/main.py
@@ -3,10 +3,17 @@ from flask import request
 
 app = Flask(__name__)
 
+
 @app.route('/', methods=['GET'])
 def add_message():
     content = request.get_json()
-    print(content)
-    genome1 = content["genomeUNK"]
-    print (genome1)
-    return genome1
+    unk = content["genomeUNK"]
+    genomes = content["genomes"]
+
+    truesIndexes = []
+
+    for i, genome in enumerate(genomes):
+        if unk == genome:
+            truesIndexes.append(i)
+
+    return str(truesIndexes)
