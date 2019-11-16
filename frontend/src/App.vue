50332cb9639834b86e53cfa1e132aeae5a18324a
Lúcás Meier <cronokirby@gmail.com>
2019-11-16 16:03:25 +0100

    Remove unnecessary things

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,28 +1,27 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>{{ message }}</div>
+    <div>
+      <input v-model="message" />
+    </div>
+    <h>Reversed</h>
+    <div>{{ reverseMessage }}</div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data() {
+    return { message: 'Hello!' };
+  },
+  computed: {
+    reverseMessage() {
+      return this.message + 'Lol';
+    }
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
