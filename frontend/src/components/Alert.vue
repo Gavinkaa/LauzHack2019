f3ac482f0fa4d2d22c9ac6363c0f1c3ae442ebe5
Lúcás Meier <cronokirby@gmail.com>
2019-11-16 16:21:38 +0100

    Add Alert card

---
+++ frontend/src/components/Alert.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h3>Alert!</h3>
+    <p>
+        Pathogen {{ pathogen }} detected in room {{ room }}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Alert',
+  props: ['pathogen', 'room'],
+};
+</script>
