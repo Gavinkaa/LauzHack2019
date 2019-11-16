b312c4c7c9f8ee1f47d0405ae86b953f9cc0df51
Gavinkaa <ludovic.burnier@epfl.ch>
2019-11-16 15:22:18 +0100

    accept multiple unkgenomes

--- mock/main.py
+++ mock/main.py
@@ -9,13 +9,17 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def add_message():
     content = request.get_json()
-    unk = content["genomeUNK"]
+    unks = content["genomeUNK"]
     genomes = content["genomes"]
 
     truesIndexes = []
 
-    for i, genome in enumerate(genomes):
-        if unk == genome:
-            truesIndexes.append(i)
+    for j, unk in enumerate(unks):
+        temp = []
+        for i, genome in enumerate(genomes):
+            if unk == genome:
+                temp.append(i)
+
+        truesIndexes.append(temp)
 
     return jsonify(truesIndexes)
