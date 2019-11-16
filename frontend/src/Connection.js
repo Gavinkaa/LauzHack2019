6fb9b2bc31c3a063a9da61eee7f7f74680e00a31
Lúcás Meier <cronokirby@gmail.com>
2019-11-16 17:33:58 +0100

    Create BFF

--- frontend/src/Connection.js
+++ frontend/src/Connection.js
@@ -3,12 +3,8 @@ class Connection {
     const socket = new WebSocket(
       `ws://localhost:1234/join?hospital=${hospital}`,
     );
-    socket.addEventListener('open', () => {
-      console.log('connected!');
-    });
     socket.addEventListener('message', event => {
       const json = JSON.parse(event.data);
-      console.log(json);
       if (json.type === 'alert') {
         this.alert(json.details);
       }
