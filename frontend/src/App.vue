f5747a35e596e3357f2a3bf47202e7733517f646
Lúcás Meier <cronokirby@gmail.com>
2019-11-16 18:44:46 +0100

    Nice connect view

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,14 @@
 <template>
-  <div id="app" class="bg-red-700">
-    <div v-if="!connection">
-      <h3 class="bg-red text-blue">Hospital:</h3>
-      <input v-model="hospital" />
-      <button v-on:click="connect">Connect</button>
+  <div id="app" class="h-screen m-0 bg-gray-200 p-12">
+    <div v-if="!connection" class="bg-white rounded-lg w-1/4 p-8 shadow-lg">
+      <h3 class="text-2xl font-bold">Hospital Name:</h3>
+      <div class="h-1/2">
+        <input class="h-full text-base p-2 my-4 border rounded shadow" v-model="hospital" />
+      </div>
+      <button
+        class="bg-blue-500 hover:bg-blue-700 text-base text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+        v-on:click="connect"
+      >Connect</button>
     </div>
     <Alert v-for="{pathogen, item} of alerts" :key="pathogen" :pathogen="pathogen" :item="item" />
   </div>
