77fd3755d48fa7bf0f80d2fb690d8f6ec7e549fb
Gavinkaa <31143057+Gavinkaa@users.noreply.github.com>
2019-11-17 03:54:03 +0100

    Auto completion + dropdown (#22)
    * autocomplete works on rooms names
    
    * autocompletion + dropdown implemented

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
+import Autocomplete from '@trevoreyre/autocomplete-vue'
+import '@trevoreyre/autocomplete-vue/dist/style.css'
 
 Vue.config.productionTip = false;
+Vue.use(Autocomplete)
+
 
 const vue = new Vue({
   router,
