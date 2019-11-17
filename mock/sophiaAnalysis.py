ff8959018cd9c63b496f1fe52c5dee0383a9d044
caillouc <pierre.colson@epfl.ch>
2019-11-17 03:19:56 +0100

    random output mock update

--- mock/sophiaAnalysis.py
+++ mock/sophiaAnalysis.py
@@ -1,27 +1,28 @@
 from flask import Flask
 from flask import request
 from flask import jsonify
+import random
 
 
 app = Flask(__name__)
 
+allName = ["VIH", "HUMAN", "PLANTES", "RHUMES", "VIRUS", "CANCER", "ANIMAL"] 
 
 @app.route('/', methods=['GET'])
 def add_message():
-    content = request.get_json()
-    unks = content["genomeUNK"]
-    genomes = content["pathogens"]
+    args = request.get_json()
+    ret = []
+    for _ in args:
+        depth = random.randint(0, 50)
+        index = random.randint(0, len(allName)-1)
+        name = allName[index]
+        if name == "VIH" or name == "VIRUS" or name == "CANCER":
+            danger = 1
+        else:
+            danger = 0
+        temp = {"name": name, "dangerous": danger, "depth": depth}
+        ret.append(temp)
+    
+    return {"species": ret}
 
-    truesIndexes = []
-
-    for j, unk in enumerate(unks):
-        temp = []
-        for i, genome in enumerate(genomes):
-            if unk == genome:
-                temp.append(i)
-
-        truesIndexes.append(temp)
-
-    return jsonify(truesIndexes)
-
-app.run ("0.0.0.0", port=5001)
+app.run ("0.0.0.0")
