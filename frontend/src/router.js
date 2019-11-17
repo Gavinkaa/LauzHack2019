6a80261413f586851fb20f5fa2980d731e23ac88
Lúcás Meier <cronokirby@gmail.com>
2019-11-17 01:01:24 +0100

    Formatting

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,42 +1,41 @@
-import Vue from 'vue'
+import Vue from 'vue';
 import Router from 'vue-router';
-import Maps from './components/Maps.vue'
-import Login from './components/Login.vue'
-import Alerts from './components/Alerts.vue'
+import Maps from './components/Maps.vue';
+import Login from './components/Login.vue';
+import Alerts from './components/Alerts.vue';
 
-Vue.use(Router)
+Vue.use(Router);
 
 const router = new Router({
-    routes: [
-        {
-            path: '/',
-            redirect: '/login'
-        },
-        {
-            path: '/login',
-            name: 'Login',
-            component: Login
-        },
-        {
-            path: '/alerts',
-            name: 'Alerts',
-            component: Alerts
-        },
-        {
-            path: '/maps',
-            name: 'Maps',
-            component: Maps
-        }
-    ]
+  routes: [
+    {
+      path: '/',
+      redirect: '/login',
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login,
+    },
+    {
+      path: '/alerts',
+      name: 'Alerts',
+      component: Alerts,
+    },
+    {
+      path: '/maps',
+      name: 'Maps',
+      component: Maps,
+    },
+  ],
 });
 
-
 router.beforeEach((to, from, next) => {
-    if (router.app.connected === false && to.name !== 'Login') {
-        next('/login')
-    } else {
-        next()
-    }
-})
+  if (router.app.connected === false && to.name !== 'Login') {
+    next('/login');
+  } else {
+    next();
+  }
+});
 
 export default router;
