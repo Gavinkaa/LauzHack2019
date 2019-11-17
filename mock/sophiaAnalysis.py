05257d3008c2c19a02ae55fbd58a396264394efc
Kajetan Pyszkowski <kajetan.pyszkowski@gmail.com>
2019-11-17 05:22:04 +0100

    Finished better Sophia implementation

--- mock/sophiaAnalysis.py
+++ mock/sophiaAnalysis.py
@@ -2,27 +2,78 @@ from flask import Flask
 from flask import request
 from flask import jsonify
 import random
+import json
 
+class TrieNode(object):
+
+    def __init__(self, char, depth, specie, danger):
+        self.char = char
+        self.children = []
+        self.word_finished = False
+        self.depth = depth
+        self.specie = specie
+        self.danger = danger
+
+
+def add(root, word, specie, danger):
+    node = root
+    for char in word:
+        found_in_child = False
+        for child in node.children:
+            if child.char == char:
+                node = child
+                found_in_child = True
+                break
+        if not found_in_child:
+            new_node = TrieNode(char, node.depth+1, specie, danger)
+            node.children.append(new_node)
+            node = new_node
+    node.word_finished = True
+
+def find_prefix(root, prefix):
+
+    parentsSpecie = None
+    parentsDang = False
+    node = root
+    size = 1
+    if not root.children:
+        return 0, 0, False
+    for char in prefix:
+        size += 1
+        char_not_found = True
+        for child in node.children:
+            if child.char == char:
+                if node.specie != None:
+                    print("changing to : ", node.specie)
+                    parentsSpecie = node.specie
+                parentsDang = node.danger
+                char_not_found = False
+                node = child
+                break
+        if char_not_found:
+            return node.depth, parentsSpecie, parentsDang
+    return node.depth, parentsSpecie, parentsDang
+
+
+#POPULATE TRIE 
+with open("species.json", 'r') as file:
+    data = json.load(file)
+root = TrieNode('*', 0, "Living", False)
+for name, data in data.items():
+    add(root, data[0], name, data[1])
 
 app = Flask(__name__)
-
-allName = ["VIH", "HUMAN", "PLANTES", "RHUMES", "VIRUS", "CANCER", "ANIMAL"] 
 
 @app.route('/', methods=['GET'])
 def add_message():
     args = request.get_json()
-    ret = []
-    for _ in args:
-        depth = random.randint(0, 50)
-        index = random.randint(0, len(allName)-1)
-        name = allName[index]
-        if name == "VIH" or name == "VIRUS" or name == "CANCER":
-            danger = 1
+    res = []
+    for sequence in args:
+        depth, parent, dangerous = find_prefix(root, sequence)
+        if dangerous:
+            res.append({'name':parent, 'dangerous': 1, 'depth':depth})
         else:
-            danger = 0
-        temp = {"name": name, "dangerous": danger, "depth": depth}
-        ret.append(temp)
-    
-    return {"species": ret}
+            res.append({'name':parent, 'dangerous': 0, 'depth':depth})
+    return {"species": res}
 
 app.run ("0.0.0.0")
