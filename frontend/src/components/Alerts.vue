fee830ae46ed9bbe80c215f3db1827f338391f34
Gavinkaa <ludovic.burnier@epfl.ch>
2019-11-17 02:14:07 +0100

    autocomplete works on rooms names

--- frontend/src/components/Alerts.vue
+++ frontend/src/components/Alerts.vue
@@ -4,6 +4,7 @@
       <span class="uppercase font-bold">Filter by:</span>
       <input class="px-2 mx-2 rounded border shadow" v-model="typeselect" />
       <input class="px-2 mx-2 rounded border shadow" v-model="select" />
+      <autocomplete :search="search" placeholder="input"></autocomplete>
     </div>
     <Alert
       v-for="({pathogen, room}, index) in filtered"
@@ -14,24 +15,49 @@
   </div>
 </template>
 
+
+
 <script>
 import Alert from './Alert';
+import Autocomplete from '@trevoreyre/autocomplete-vue';
 
 export default {
   name: 'Alerts',
-  components: { Alert },
+  components: {
+    Alert,
+    Autocomplete,
+  },
   props: ['alerts'],
   data() {
     return { select: '', typeselect: '' };
+  },
+  methods: {
+    search(input) {
+      if (input.length < 1) {
+        return [];
+      }
+      return this.alerts
+        .map(x => x.room)
+        .filter(alert => {
+          console.log(alert);
+          return alert.toLowerCase().startsWith(input.toLowerCase());
+        });
+    },
   },
   computed: {
     filtered() {
       return this.alerts.filter(x => {
         const lowered = this.typeselect.toLowerCase();
         if (lowered === 'room') {
-          return this.select === '' || x.room.toLowerCase() === this.select.toLowerCase();
+          return (
+            this.select === '' ||
+            x.room.toLowerCase() === this.select.toLowerCase()
+          );
         } else if (lowered === 'pathogen') {
-          return this.select === '' || x.pathogen.toLowerCase() === this.select.toLowerCase();
+          return (
+            this.select === '' ||
+            x.pathogen.toLowerCase() === this.select.toLowerCase()
+          );
         } else {
           return true;
         }
