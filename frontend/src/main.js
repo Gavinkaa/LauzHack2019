59501d07034583fe1d5c9d0d258b79c3f8f57269
Lúcás Meier <cronokirby@gmail.com>
2019-11-17 04:12:33 +0100

    Test

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,12 +1,13 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
-import Autocomplete from '@trevoreyre/autocomplete-vue'
-import '@trevoreyre/autocomplete-vue/dist/style.css'
+import Autocomplete from '@trevoreyre/autocomplete-vue';
+import VuePIXI from 'vue-pixi';
+import '@trevoreyre/autocomplete-vue/dist/style.css';
 
 Vue.config.productionTip = false;
-Vue.use(Autocomplete)
-
+Vue.use(Autocomplete);
+Vue.use(VuePIXI);
 
 const vue = new Vue({
   router,
