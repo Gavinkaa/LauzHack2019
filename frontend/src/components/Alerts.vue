1071fd46bb6dcb045d7dee35617e5e9f041bb1fa
caillouc <48051170+caillouc@users.noreply.github.com>
2019-11-17 05:36:15 +0100

    add outbreak detection (#25)
    * Auto completion + dropdown (#22)
    
    * autocomplete works on rooms names
    
    * autocompletion + dropdown implemented
    
    * Mock upgraded (#23)
    
    * random output mock update
    
    * Finished better Sophia implementation

--- frontend/src/components/Alerts.vue
+++ frontend/src/components/Alerts.vue
@@ -1,9 +1,17 @@
 <template>
   <div class="text-center">
-    <div class="text-lg lg:text-xl bg-white rounded-lg mb-4 w-full lg:w-1/2 shadow mx-auto p-4">
-      <span class="uppercase font-bold">Filter by:</span>
-      <input class="px-2 mx-2 rounded border shadow" v-model="typeselect" />
-      <input class="px-2 mx-2 rounded border shadow" v-model="select" />
+    <div class="text-lg lg:text-xl bg-white rounded-lg mb-4 w-3/4 lg:w-1/2 shadow mx-auto p-4">
+      <div class="text-align-bottom">
+        <div class="uppercase font-bold">Filter by:</div>
+        <dropdown
+          :options="filterlist"
+          :selected="typeselect"
+          v-on:updateOption="methodToRunOnSelect"
+        ></dropdown>
+      </div>
+      <div class="w-1/2 mx-auto my-2">
+        <autocomplete :search="search" placeholder="input" @submit="handleSubmit"></autocomplete>
+      </div>
     </div>
     <Alert
       v-for="({pathogen, room}, index) in filtered"
@@ -16,22 +24,57 @@
 
 <script>
 import Alert from './Alert';
+import dropdown from 'vue-dropdowns';
+import Autocomplete from '@trevoreyre/autocomplete-vue';
 
 export default {
   name: 'Alerts',
-  components: { Alert },
+  components: {
+    Alert,
+    Autocomplete,
+    dropdown: dropdown,
+  },
   props: ['alerts'],
   data() {
-    return { select: '', typeselect: '' };
+    return {
+      select: undefined,
+      filterlist: [{ name: 'room' }, { name: 'pathogen' }],
+      typeselect: {
+        name: 'Choose a filter',
+      },
+    };
+  },
+  methods: {
+    methodToRunOnSelect(payload) {
+      this.typeselect = payload;
+    },
+    search(input) {
+      if (input.length < 1) {
+        return [];
+      }
+      return this.alerts
+        .map(x => (this.typeselect.name === 'room' ? x.room : x.pathogen))
+        .filter(alert => {
+          return alert.toLowerCase().startsWith(input.toLowerCase());
+        });
+    },
+    handleSubmit(result) {
+      this.select = result;
+    },
   },
   computed: {
     filtered() {
       return this.alerts.filter(x => {
-        const lowered = this.typeselect.toLowerCase();
+        const lowered = this.typeselect['name'].toLowerCase();
         if (lowered === 'room') {
-          return this.select === '' || x.room.toLowerCase() === this.select.toLowerCase();
+          return (
+            !this.select || x.room.toLowerCase() === this.select.toLowerCase()
+          );
         } else if (lowered === 'pathogen') {
-          return this.select === '' || x.pathogen.toLowerCase() === this.select.toLowerCase();
+          return (
+            !this.select ||
+            x.pathogen.toLowerCase() === this.select.toLowerCase()
+          );
         } else {
           return true;
         }
--- mock/sophiaAnalysis.py
+++ mock/sophiaAnalysis.py
@@ -1,27 +1,79 @@
 from flask import Flask
 from flask import request
 from flask import jsonify
+import random
+import json
 
+class TrieNode(object):
+
+    def __init__(self, char, depth, specie, danger):
+        self.char = char
+        self.children = []
+        self.word_finished = False
+        self.depth = depth
+        self.specie = specie
+        self.danger = danger
+
+
+def add(root, word, specie, danger):
+    node = root
+    for char in word:
+        found_in_child = False
+        for child in node.children:
+            if child.char == char:
+                node = child
+                found_in_child = True
+                break
+        if not found_in_child:
+            new_node = TrieNode(char, node.depth+1, specie, danger)
+            node.children.append(new_node)
+            node = new_node
+    node.word_finished = True
+
+def find_prefix(root, prefix):
+
+    parentsSpecie = None
+    parentsDang = False
+    node = root
+    size = 1
+    if not root.children:
+        return 0, 0, False
+    for char in prefix:
+        size += 1
+        char_not_found = True
+        for child in node.children:
+            if child.char == char:
+                if node.specie != None:
+                    print("changing to : ", node.specie)
+                    parentsSpecie = node.specie
+                parentsDang = node.danger
+                char_not_found = False
+                node = child
+                break
+        if char_not_found:
+            return node.depth, parentsSpecie, parentsDang
+    return node.depth, parentsSpecie, parentsDang
+
+
+#POPULATE TRIE 
+with open("species.json", 'r') as file:
+    data = json.load(file)
+root = TrieNode('*', 0, "Living", False)
+for name, data in data.items():
+    add(root, data[0], name, data[1])
 
 app = Flask(__name__)
 
-
 @app.route('/', methods=['GET'])
 def add_message():
-    content = request.get_json()
-    unks = content["genomeUNK"]
-    genomes = content["pathogens"]
+    args = request.get_json()
+    res = []
+    for sequence in args:
+        depth, parent, dangerous = find_prefix(root, sequence)
+        if dangerous:
+            res.append({'name':parent, 'dangerous': 1, 'depth':depth})
+        else:
+            res.append({'name':parent, 'dangerous': 0, 'depth':depth})
+    return {"species": res}
 
-    truesIndexes = []
-
-    for j, unk in enumerate(unks):
-        temp = []
-        for i, genome in enumerate(genomes):
-            if unk == genome:
-                temp.append(i)
-
-        truesIndexes.append(temp)
-
-    return jsonify(truesIndexes)
-
-app.run ("0.0.0.0", port=5001)
+app.run ("0.0.0.0")
